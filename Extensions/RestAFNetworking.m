348ba4fb1f9268ec2de01cb1887b0dee329f1c99
Aurelien <aurelien.scelles@display-interactive.com>
2015-11-02 11:49:20 +0100

    change thread management

--- Extensions/RestAFNetworking.m
+++ Extensions/RestAFNetworking.m
@@ -34,7 +34,7 @@
 
 -(instancetype)initWithBaseURL:(NSURL *)url {
     self = [super initWithBaseURL:url];
-    self.completionQueue = dispatch_queue_create("rest.manager.afnetworking.parse", 0);
+    self.completionQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
     return self;
 }
 
